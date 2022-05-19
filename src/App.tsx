ed0c39ffc658662bbda045c12cea827dd8ec5b17
borasanuk <bora@aeontech.io>
2022-05-20 01:01:52 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="tinas-app-container">
+      <div className="tinas-navbar">
+        <img
+          className="tinas-navbar-logo"
+          src="https://i.ibb.co/XVNFSWJ/logo-white.png"
+          alt="logo-white"
+        />
+        <div className="tinas-navbar-item tinas-navbar-item-selected">home</div>
+        <div className="tinas-navbar-item">about</div>
+        <div className="tinas-navbar-item">contact</div>
+      </div>
+      <div className="tinas-hero">
+        <div className="tinas-hero-bg-container">
+          <img
+            className="tinas-hero-bg"
+            src="https://i.ibb.co/zZC8sF9/whitney-wright-q-Kx-ELx-IH5t8-unsplash.jpg"
+            alt="whitney-wright-q-Kx-ELx-IH5t8-unsplash"
+          />
+        </div>
+        <div className="tinas-hero-title">
+          tina's bananas
+          <br />
+          about banana bread.
+        </div>
+      </div>
     </div>
   );
 }
